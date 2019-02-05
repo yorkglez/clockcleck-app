d6e9856168a0bcf92a910caf62cf59bbd5e859ae
yorkglez <bobo_12yors@hotmail.com>
2019-02-04 21:53:12 -0600

    comit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -20,47 +20,57 @@ export class LoginComponent implements OnInit {
               }
 
   ngOnInit(){
+      /* call isLoggedIn service */
       this._authService.isLoggedIn().subscribe(resp=> {
+      /* Validate if user is logged */
       if(resp.type == 'admin' && resp.logged)
-        this.router.navigate(['users'])
+        this.router.navigate(['users']) //redirect to users page
       else if(resp.type == 'normal' && resp.logged)
-        this.router.navigate(['reports'])
+        this.router.navigate(['reports']) //redirect to reports page
       else if(resp.type == 'teacher' && resp.logged)
-        this.router.navigate(['porfileteacher'])
+        this.router.navigate(['porfileteacher']) //redirect to porfileteacher page
+      /* user isn't logged*/
       else{
-        localStorage.clear()
-        this.router.navigate(['login'])
-        localStorage.setItem('type','user')
+        localStorage.clear() //Clear localstorage
+        this.router.navigate(['login']) //redirect to login page
+        localStorage.setItem('type','user') //Create session in localStorage
       }
     })
+
     this.model['email']  = ''
     this.model['password']  = ''
 }
 
-loginUser(event){
-  if (this.model['email'] !='' && this.model['password'] !='') {
-    this._authService.authUser(this.model).subscribe(resp => {
-      if (resp.success) {
-        if(resp.type == 'admin'){
-          this.router.navigate(['users'])//Redirect to home admin
-          this._authService.setLoggedIn(true)
+  loginUser(){
+    /* Validate email and password */
+    if(this.model['email'] !='' && this.model['password'] !='') {
+      /* Call authUser service */
+      this._authService.authUser(this.model).subscribe(resp => {
+        /* Validate response*/
+        if (resp.success) {
+          /* Validate user type */
+          if(resp.type == 'admin'){
+            this.router.navigate(['users'])//Redirect to home admin
+            this._authService.setLoggedIn(true)
+          }
+          else if(resp.type == 'normal'){
+            this.router.navigate(['reports'])//Redirect to home user normal
+            this._authService.setLoggedIn(true)
+          }
+          localStorage.clear();  //Clear localstorage
+          /* Create localStorage with user data */
+          localStorage.setItem('extension',resp.extension)
+          localStorage.setItem('username',resp.username)
+          localStorage.setItem('email',resp.email)
+          localStorage.setItem('type',resp.type)
         }
-        else if(resp.type == 'normal'){
-          this.router.navigate(['reports'])//Redirect to home user normal
-          this._authService.setLoggedIn(true)
+        /* Login error */
+        else{
+          this._authService.setLoggedIn(false)
+          this.isError = true
         }
-        localStorage.clear();
-        localStorage.setItem('extension',resp.extension)
-        localStorage.setItem('username',resp.username)
-        localStorage.setItem('email',resp.email)
-        localStorage.setItem('type',resp.type)
-      }
-      else{
-        this._authService.setLoggedIn(false)
-        this.isError = true
-      }
-    })
+      })
+    }
   }
-}
 
 }
